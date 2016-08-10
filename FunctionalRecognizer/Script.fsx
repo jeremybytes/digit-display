b8b05d9b3f8f711a2d747b9e5c692498c40d4342
Jeremy Clark <jeremy@jeremybytes.com>
2016-08-10 16:17:33 -0700

    Experiments with blur classifier. No improvement.

--- FunctionalRecognizer/Script.fsx
+++ FunctionalRecognizer/Script.fsx
@@ -29,6 +29,37 @@ let euclideanDistance (pixels1,pixels2) =
     |> Array.map (fun (x,y) -> pown (x-y) 2)
     |> Array.sum
 
+let horizontalBlurArray (arr:int[]) =
+    let res = Array.create arr.Length 0
+    res.[0] <- (arr.[0] + arr.[1]) / 2
+    res.[arr.Length-1] <- (arr.[arr.Length-2] + arr.[arr.Length-1]) /2
+    for i in 1 .. arr.Length-2 do
+        res.[i] <- (arr.[i-1] + arr.[i] + arr.[i+1]) / 3
+    res
+
+let intSqrt x = int (Math.Sqrt(float x))
+
+let centerBlurArray (arr:int[]) =
+    let res = Array.create arr.Length 0
+    let rowLen = intSqrt res.Length
+    for i in 0 .. rowLen-1 do
+        for j in 1 .. rowLen-2 do
+            let coord = i*rowLen + j
+            let prev = i*(rowLen-1) + j
+            let next = i*(rowLen+1) + j
+            let prevRow = if (prev >= 0) then prev else coord
+            let nextRow = if (next < res.Length) then next else coord
+            res.[coord] <- (
+                arr.[prevRow-1] + arr.[prevRow] + arr.[prevRow+1] +
+                arr.[coord-1] + (arr.[coord] * 4) + arr.[coord+1] +
+                arr.[nextRow-1] + arr.[nextRow] + arr.[nextRow+1]) / 12
+    res
+
+let blurDistance (pixels1,pixels2:int[]) =
+    let blur1 = centerBlurArray pixels1
+    let blur2 = centerBlurArray pixels2
+    manhattanDistance(blur1,blur2)
+
 let train (trainingset:Observation[]) (dist:Distance) =
     let classify (pixels:int[]) =
         trainingset
@@ -53,8 +84,11 @@ let evaluate validationData classifier =
 
 let manhattanClassifier = train trainingData manhattanDistance
 let euclideanClassifier = train trainingData euclideanDistance
+let blurClassifier = train trainingData blurDistance
 
 printfn "Manhattan"
 evaluate validationData manhattanClassifier
+printfn "BlurManhattan"
+evaluate validationData blurClassifier
 printfn "Euclidean"
 evaluate validationData euclideanClassifier
