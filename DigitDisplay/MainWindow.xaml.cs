191e53a66012cee2dbc0e9f9b29313b560513dec
Yuriy Lyeshchenko <yuriylsh@live.com>
2017-12-31 18:14:45 -0600

    Refactoring observation loading into a separate project

--- DigitDisplay/MainWindow.xaml.cs
+++ DigitDisplay/MainWindow.xaml.cs
@@ -1,21 +1,14 @@
 ﻿using DigitLoader;
-using Microsoft.FSharp.Core;
-using System;
 using System.Configuration;
-using System.Drawing;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace DigitDisplay
 {
     public partial class MainWindow : Window
     {
-        SolidColorBrush redBrush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 150, 150));
-        SolidColorBrush whiteBrush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 255, 255));
+        public static readonly SolidColorBrush RedBrush = new SolidColorBrush(Color.FromRgb(255, 150, 150));
+        public static readonly SolidColorBrush WhiteBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
 
         public MainWindow()
         {
@@ -40,21 +33,6 @@ namespace DigitDisplay
                 "Manhattan Classifier", Recognizer.manhattanClassifier,
                 rawData);
             RightPanel.Children.Add(manhattanRecognizer);
-
-            //var euclideanRecognizer = new RecognizerControl(
-            //    "Euclidean Classifier", Recognizer.euclideanClassifier,
-            //    rawData);
-            //RightPanel.Children.Add(euclideanRecognizer);
-
-            //var nullRecognizer = new RecognizerControl(
-            //    "Null Classifier", (FSharpFunc<int[], string>)Recognizer.nullClassifier,
-            //    rawData);
-            //RightPanel.Children.Add(nullRecognizer);
-
-            //var firstPixelRecognizer = new RecognizerControl(
-            //    "FirstPixel Classifier", (FSharpFunc<int[], string>)Recognizer.firstPixelClassifier,
-            //    rawData);
-            //LeftPanel.Children.Add(firstPixelRecognizer);
         }
     }
 }
---
+++ ObservationLoader/DataReader.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace ObservationLoader
+{
+    public class DataReader
+    {
+        public static Observation[] ReadObservations(string dataPath, int offset = 0, int? count = null)
+        {
+            var data = File.ReadAllLines(dataPath).Skip(1 + offset);
+            data = count.HasValue ? data.Take(count.Value) : data;
+            return data.Select(ObservationFactory).ToArray();
+        }
+
+        private static Observation ObservationFactory(string data)
+        {
+            var commaSeparated = data.Split(',');
+           
+            return commaSeparated.Length == 785 
+                ? new Observation(commaSeparated[0], IntStringsAsInts(commaSeparated, 1)) 
+                : new Observation("T", IntStringsAsInts(commaSeparated, 0));
+        }
+
+        private static int[] IntStringsAsInts(string[] intStrings, int offset)
+        {
+            var result = new int[intStrings.Length - offset];
+            for (var i = offset; i < result.Length; i++)
+            {
+                result[i] = int.Parse(intStrings[i], NumberStyles.Integer);
+            }
+
+            return result;
+        }
+    }
+}
